df543626e2a6580087910ab49ebf87c0ce453ca4
jmpargana <jmpargana@stud.uni-frankfurt.de>
2020-03-23 16:28:18 +0100

    backend connecting to mongo and npm run dev running backend and frontend concurrently

--- config/db.js
+++ config/db.js
@@ -2,19 +2,18 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connectDB = async() => {
-    try {
-        await mongoose.connect(
-            db, {
-                useNewUrlparser: true
-            }
-        );
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
 
-        console.log('MongoDB is Connected...');
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-    }
+    console.log('MongoDB is Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
